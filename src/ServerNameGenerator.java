e716c5203f7b61162adc8b7e00fcbf037b71fcb0
sam's macbook <samuel2.martinez2@gmail.com>
2022-06-17 16:57:30 -0500

    still working

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,32 @@
+import org.w3c.dom.ls.LSOutput;
+
+import javax.crypto.spec.PSource;
+import java.util.Arrays;
+
+public class ServerNameGenerator {
+
+
+    String[] adj = {"acidic", "adorable", "aggressive","alert", "brave", "cruel", "friendly", "nasty", "perfect", "lazy" };
+    String[] Noun = {"bed", "cat","movie","train","book","phone","pen","king","hero","water"};
+
+
+public static int random(int arr){
+
+    String Arrays.toString(arr);
+
+}
+
+
+
+
+
+    public static void main(String[] args){
+
+    random("adj");
+        System.out.println(random("adj"));
+
+    }
+
+
+   //**************** //end of Class********
+}
