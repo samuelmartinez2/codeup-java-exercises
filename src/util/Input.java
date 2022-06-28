d7c16a277bcf020d83e5772e83fea9b431cddead
sam's macbook <samuel2.martinez2@gmail.com>
2022-06-28 06:20:39 -0500

    updated input

--- src/util/Input.java
+++ src/util/Input.java
@@ -2,6 +2,7 @@ package util;
 
 import shapes.Circle;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Input {
@@ -12,6 +13,11 @@ public class Input {
         System.out.println("what is your name");
         return scanner.nextLine();
     }
+    public String getString(String input) {
+        System.out.println(input);
+        return scanner.nextLine();
+    }
+
 
     public boolean yesNo() {
         System.out.println("would you like to play a game?");
@@ -25,36 +31,89 @@ public class Input {
 
     public int getInt() {
 //        System.out.println("give me a number");
-        return scanner.nextInt();
-    }
-
-    public int getInt(int min, int max) {
-        System.out.printf("give me a number from %d - %d", min, max);
-        int userinput = scanner.nextInt();
-        if (userinput >= min && userinput <= max) {
-            return userinput;
-        } else {
-            System.out.println("can;\'t count");
-            return getInt(min, max);
+        String input = getString("give me a number");
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return getInt();
         }
     }
 
+    public int getInteger(int min, int max) {
+        String input = getString("give me a number between " + min + " and " + max + ".");
+
+        try {
+            int Input = Integer.parseInt(input);
+            if (Input >= min && Input <= max) {
+                return Input;
+            } else {
+                System.out.println("can;\'t count?");
+                return getInteger(min, max);
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return getInteger(min, max);
+        }
+    }
 
     public double getDouble() {
-        System.out.println("give me a number");
-        return scanner.nextDouble();
+
+        String input= getString("give me a number");
+        try {
+            return Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return getDouble();
+        }
     }
 
     public double getDouble(double min, double max) {
-        System.out.printf("give me a number from %f - $f", min, max);
-        double userinput = scanner.nextDouble();
-        if (userinput > min && userinput < max) {
-            return userinput;
-        } else {
-            System.out.println("can;\'t count");
-            return getDouble(min, max);
+        String input = getString("give me a number between " + min + " and " + max + ".");
+        try {
+            double Input = Double.parseDouble(input);
+            if (Input > min && Input < max) {
+                return Input;
+            } else {
+                System.out.println("can;\'t count?");
+                return getDouble(min, max);
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return getDouble();
         }
     }
+// got this from walk trough, this was the bonus.
+    public long getHex() {
+        System.out.println("Input hexadecimal number");
+        while (true) {
+            try {
+                long hex = Long.valueOf(scanner.nextLine(), 16);
+                System.out.println("Input acceptable");
+                return hex;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid. Input not a hexadecimal. Try again.");
+            }
+        }
+    }
+
+    public long getBinary() {
+        System.out.println("Input binary number");
+        while (true) {
+            try {
+                long bin = Long.valueOf(scanner.nextLine(), 2);
+                System.out.println("Input acceptable");
+                return bin;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid. Input not in binary. Try again.");
+            }
+        }
+    }
+
+
+
+
+
 
 
 
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -2,12 +2,27 @@ package util;
 
 public class InputTest {
     public static void main(String[] args) {
+//        try{
+//            Double.parseDouble(getString()) = getDouble();
+//            System.out.println("ok"+ Double.valueOf(getString()));
+//        } catch (Exception e) {
+//            System.out.println(" HERE "+ e. getMessage());
+//            System.out.println("more details");
+//            e.printStackTrace();
+//        }
+
 
         Input answer = new Input();
-        System.out.println(answer.getString());
 
-        System.out.println(answer.yesNo());
-        System.out.println(answer.getInt(1,10));
+
+//        System.out.println(answer.getString());
+//
+//        System.out.println(answer.yesNo());
+        System.out.println(answer.getInt());
+        System.out.println(answer.getInteger(5,20));
+        System.out.println(answer.getDouble());
+        System.out.println(answer.getDouble(5,10));
+
 
     }
 }
